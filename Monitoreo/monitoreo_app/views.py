861f7fc884baca8c09e260f3036e9045d514f16d
Juanluis22 <103545943+Juanluis22@users.noreply.github.com>
2023-08-19 19:58:11 -0400

    login

--- Monitoreo/monitoreo_app/views.py
+++ Monitoreo/monitoreo_app/views.py
@@ -1,3 +1,4 @@
+from pyexpat.errors import messages
 from django.shortcuts import render
 
 from django.http import HttpResponse
@@ -7,3 +8,21 @@ def index(request):
 
 def about(request):
     return render(request, 'monitoreo_app/about.html')
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('nombre_de_la_vista_después_del_login')
+        else:
+            # Usuario no válido, puedes mostrar un mensaje de error
+            messages.error(request, 'Correo o contraseña incorrectos, prueba de nuevo.')
+
+    return render(request, 'nombre_de_tu_template_login.html')
