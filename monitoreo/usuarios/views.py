0c20eae1d01fe7c64c141bd63bc375f0602fcde1
Vicente Farias <armando3443op@gmail.com>
2023-08-24 23:07:57 -0400

    Vertientes asociadas a los habitantes (y la comunidad)

--- monitoreo/usuarios/views.py
+++ monitoreo/usuarios/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView
-
-# Create your views here.
+from django.views.generic import TemplateView
+from nucleo.models import habitantes
 
 class Selector(TemplateView):
-    
-    template_name = 'selector/selector.html'  
-    context_object_name = 'listaUser'
+    template_name = 'selector/selector.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # Obtener el habitante actual
+        habitante = habitantes.objects.get(usuario=self.request.user)
+
+        # Obtener la comunidad y las vertientes asociadas al habitante
+        comunidad_habitante = habitante.comunidad
+        context['comunidad_habitante'] = comunidad_habitante
+        if comunidad_habitante:
+            # Obtén las vertientes asociadas a la comunidad del habitante
+            vertientes_comunidad = comunidad_habitante.vertiente_set.all()
+            context['vertientes_comunidad'] = vertientes_comunidad
+        
+        return context
